92ea89c743f9cef1df909497f43886682541aa3a
collaroid <su.liangbo@gmail.com>
2014-11-02 21:29:12 +0800

    comment on detail page

--- Utils.js
+++ Utils.js
@@ -2,6 +2,7 @@
  * Created by MacBookAir on 10/28/14.
  */
 var moment = require('moment');
+var crypto = require('crypto');
 
 exports.timestamp2Date = function(timestamp){
     return new moment(timestamp);
@@ -10,4 +11,13 @@ exports.timestamp2Date = function(timestamp){
 exports.date2Timestamp = function(date){
     return new moment(date).valueOf();
 };
+
+/**
+ * MD5加密
+ * @param data
+ * @returns {*}
+ */
+exports.md5 = function(data) {
+    return crypto.createHash('md5').update(data).digest('hex').toLowerCase();
+}
 
--- routes/postRouter.js
+++ routes/postRouter.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var postService = require('../service/postService');
 
 /* GET one post info. */
-router.get('/detail/:postId', function(req, res) {
+router.get('/:postId', function(req, res) {
   postService.queryOnePost(req.params.postId,function(err, data){
       if(err){
           res.send("query " + req.params.postId + "failed!");
@@ -12,6 +12,14 @@ router.get('/detail/:postId', function(req, res) {
           console.log(data);
       }
   });
+});
+
+
+/* POST to save one comment info. */
+router.post('/comment', function(req, res) {
+    postService.saveComment(req.body.postId, req.body.postContent,function(err, data){
+        res.send(data);
+    });
 });
 
 module.exports = router;
