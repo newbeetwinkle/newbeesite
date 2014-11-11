cafd757f0e79017b3c5f18a98749c18b49bf927c
collaroid <su.liangbo@gmail.com>
2014-11-12 00:43:48 +0800

    modify main page css

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,15 @@ var util = require('../utils');
 router.get('/', function(req, res) {
 	postService.queryAllPost(function(e, posts){
 		posts.forEach(function(element, index, array){
-			if(element.content && element.content.length > 100){
-				element.content = util.removeHTMLTag(element.content).substring(0, 100) + "...";
+			if(element.content){
+                var pureText = util.removeHTMLTag(element.content);
+                if(pureText.length > 500){
+                    pureText = pureText.substring(0, 500) + " [......]";
+                }
+                pureText = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + pureText;
+				element.content = pureText;
+                if(typeof(element.viewCount) == "undefined")
+                    element.viewCount = 0;
 			}
 		});
 		res.render('index',{"posts":posts,"user":req.session.user});
--- utils.js
+++ utils.js
@@ -32,10 +32,10 @@ exports.md5 = function(data) {
 
 
 exports.removeHTMLTag = function(str) {
-    str = str.replace(/<\/?[^>]*>/g,''); //去除HTML tag
-    str = str.replace(/[ | ]*\n/g,'\n'); //去除行尾空白
-    //str = str.replace(/\n[\s| | ]*\r/g,'\n'); //去除多余空行
-    str=str.replace(/&nbsp;/ig,'');//去掉&nbsp;
+    str = str.replace(/<\/?[^>]*>/g, '');       //去除HTML tag
+    str = str.replace(/[ | ]*\n/g, '\n');       //去除行尾空白
+    str = str.replace(/\n[\s| | ]*\r/g,'\n');   //去除多余空行
+    str = str.replace(/&nbsp;/ig,' ');          //去掉&nbsp;
     return str;
 }
 
