ef34fb995d34fa518b74c8d3fd2a05e98e657c70
collaroid <su.liangbo@gmail.com>
2014-11-11 23:58:24 +0800

    article summary on main page

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var postService = require('../service/postService');
-
+var util = require('../utils');
 
 
 /* GET home page. */
@@ -9,7 +9,7 @@ router.get('/', function(req, res) {
 	postService.queryAllPost(function(e, posts){
 		posts.forEach(function(element, index, array){
 			if(element.content && element.content.length > 100){
-				element.content = element.content.substring(0, 100) + "...";
+				element.content = util.removeHTMLTag(element.content).substring(0, 100) + "...";
 			}
 		});
 		res.render('index',{"posts":posts,"user":req.session.user});
